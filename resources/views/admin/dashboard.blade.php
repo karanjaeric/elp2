ef670cd7424568b12cfb6c7276040404f47e94d9
Edwinwkuria <edwin.w.kuria@gmail.com>
2019-11-10 17:08:04 +0300

    minor fixes on UI layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,116 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-  <title>Equity Leaders Program</title>
-
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fontawesome-free/css/all.min.css") }}">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/dist/css/adminlte.min.css") }}">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{ asset("/images/AdminLTELogo.png") }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">ELP Portal</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset("/images/user.png")}}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block"> Edwin Kuria</a>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="/outline" class=" active nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/summaries" class="nav-link">
-              <i class="nav-icon fas fa-align-center"></i>
-              <p>
-                Summaries
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/reports" class="nav-link">
-              <i class="nav-icon fas fa-layer-group"></i>
-              <p>
-                Reports
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/scholar" class="nav-link">
-              <i class="nav-icon fas fa-user-alt"></i>
-              <p>
-                Scholars
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/calendar" class="nav-link">
-              <i class="nav-icon fas fa-calendar-alt"></i>
-              <p>
-                Calendar
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Edwin Kuria</a></li>
-              <li class="breadcrumb-item active">Log Out</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
+@extends('admin.layouts.master')
+@section('content')
+<!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <!-- Info boxes -->
@@ -193,6 +83,75 @@
                     <div class="chart">
                       <!-- Sales Chart Canvas -->
                       <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
+                      <script>
+                       $(function () {
+
+                        'use strict'
+
+                        /* ChartJS*/
+
+                        // Get context with jQuery - using jQuery's .get() method.
+                        var salesChartCanvas = $('#salesChart').get(0).getContext('2d')
+
+                        var salesChartData = {
+                          labels  : ['1998', '2003', '2008', '2013', '2018'],
+                          datasets: [
+                            {
+                              backgroundColor     : 'rgba(210, 214, 222, 0.25)',
+                              borderColor         : 'rgba(60,141,188,0.8)',
+                              pointRadius          : false,
+                              pointColor          : '#3b8bba',
+                              pointStrokeColor    : 'rgba(60,141,188,1)',
+                              pointHighlightFill  : '#fff',
+                              pointHighlightStroke: 'rgba(60,141,188,1)',
+                              data                : [2, 48, 150, 275, 478 ]
+                            },
+                            {
+                              backgroundColor     : 'rgba(60,141,188,1)',
+                              borderColor         : 'rgba(210, 214, 222, 1)',
+                              pointRadius         : false,
+                              pointColor          : 'rgba(210, 214, 222, 1)',
+                              pointStrokeColor    : '#c1c7d1',
+                              pointHighlightFill  : '#fff',
+                              pointHighlightStroke: 'rgba(220,220,220,1)',
+                              data                : [ 1, 20, 80, 137, 245 ]
+                            },
+                          ]
+                        }
+
+                        var salesChartOptions = {
+                          maintainAspectRatio : false,
+                          responsive : true,
+                          legend: {
+                            display: false
+                          },
+                          scales: {
+                            xAxes: [{
+                              gridLines : {
+                                display : false,
+                              }
+                            }],
+                            yAxes: [{
+                              gridLines : {
+                                display : false,
+                              }
+                            }]
+                          }
+                        }
+
+                        // This will get the first returned node in the jQuery collection.
+                        var salesChart = new Chart(salesChartCanvas, { 
+                            type: 'line', 
+                            data: salesChartData, 
+                            options: salesChartOptions
+                          }
+                        )
+
+                        //---------------------------
+                        //- END MONTHLY SALES CHART -
+                        //--------------------------
+                      })
+                      </script>
                     </div>
                     <!-- /.chart-responsive -->
                   </div>
@@ -293,45 +252,5 @@
         <!-- /.row -->
       </div><!--/. container-fluid -->
     </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; Equity Leaders Program
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      {{ 2019 }}
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="{{ asset("/bower_components/admin-lte/plugins/jquery/jquery.min.js") }}" ></script>
-<!-- Bootstrap -->
-<script src="{{ asset("/bower_components/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ asset("/bower_components/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset("/bower_components/admin-lte/dist/js/adminlte.js") }}"></script>
-
-<!-- OPTIONAL SCRIPTS -->
-<script src="{{ asset("/bower_components/admin-lte/dist/js/demo.js") }}"></script>
-
-<!-- PAGE PLUGINS -->
-<!-- ChartJS -->
-<script src="{{ asset("/bower_components/admin-lte/plugins/chart.js/Chart.min.js") }}"></script>
-
-<!-- PAGE SCRIPTS -->
-<script src="{{ asset("/bower_components/admin-lte/dist/js/pages/dashboard2.js") }}"></script>
-</body>
-</html>
+@endsection
+    
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Equity Leaders Program</title>
+   <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.css")}}">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fontawesome-free/css/all.min.css") }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/dist/css/adminlte.min.css") }}">
+    <!-- fullCalendar -->
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fullcalendar/main.min.css")}}">
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-interaction/main.min.css")}}">
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-daygrid/main.min.css")}}">
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-timegrid/main.min.css")}}">
+  <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-bootstrap/main.min.css")}}">
+    <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- jQuery -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/jquery/jquery.min.js") }}" ></script>
+  <!-- Chart CSS-->
+  <script src="{{ asset("/bower_components/admin-lte/plugins/chart.js/Chart.min.js")}}"></script>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="{{ asset("/images/AdminLTELogo.png") }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">ELP Portal</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{ asset("/images/user.png")}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block"> Edwin Kuria</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="/outline" class=" active nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/summaries" class="nav-link">
+              <i class="nav-icon fas fa-align-center"></i>
+              <p>
+                Summaries
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/reports" class="nav-link">
+              <i class="nav-icon fas fa-layer-group"></i>
+              <p>
+                Reports
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/scholar" class="nav-link">
+              <i class="nav-icon fas fa-user-alt"></i>
+              <p>
+                Scholars
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/calendar" class="nav-link">
+              <i class="nav-icon fas fa-calendar-alt"></i>
+              <p>
+                Calendar
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Edwin Kuria</a></li>
+              <li class="breadcrumb-item active">Log Out</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; Equity Leaders Program
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      {{ 2019 }}
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- Bootstrap -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset("/bower_components/admin-lte/dist/js/adminlte.js") }}"></script>
+<!-- jQuery UI -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/jquery-ui/jquery-ui.min.js")}}"></script>
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{ asset("/bower_components/admin-lte/dist/js/demo.js") }}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- ChartJS -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/chart.js/Chart.min.js") }}"></script>
+
+<!-- PAGE SCRIPTS -->
+<!--<script src="{{ asset("/bower_components/admin-lte/dist/js/pages/dashboard2.js") }}"></script>-->
+
+<!-- DataTables -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/datatables/jquery.dataTables.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js")}}"></script>
+
+<!-- fullCalendar 2.2.5 -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/moment/moment.min.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/fullcalendar/main.min.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-daygrid/main.min.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-timegrid/main.min.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-interaction/main.min.js")}}"></script>
+<script src="{{ asset("/bower_components/admin-lte/plugins/fullcalendar-bootstrap/main.min.js")}}"></script>
+
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+</script>
+<!-- Page specific script -->
+<script>
+  $(function () {
+
+    /* initialize the external events
+     -----------------------------------------------------------------*/
+    function ini_events(ele) {
+      ele.each(function () {
+
+        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+        // it doesn't need to have a start or end
+        var eventObject = {
+          title: $.trim($(this).text()) // use the element's text as the event title
+        }
+
+        // store the Event Object in the DOM element so we can get to it later
+        $(this).data('eventObject', eventObject)
+
+        // make the event draggable using jQuery UI
+        $(this).draggable({
+          zIndex        : 1070,
+          revert        : true, // will cause the event to go back to its
+          revertDuration: 0  //  original position after the drag
+        })
+
+      })
+    }
+
+    ini_events($('#external-events div.external-event'))
+
+    /* initialize the calendar
+     -----------------------------------------------------------------*/
+    //Date for the calendar events (dummy data)
+    var date = new Date()
+    var d    = date.getDate(),
+        m    = date.getMonth(),
+        y    = date.getFullYear()
+
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendarInteraction.Draggable;
+
+    var containerEl = document.getElementById('external-events');
+    var checkbox = document.getElementById('drop-remove');
+    var calendarEl = document.getElementById('calendar');
+
+    // initialize the external events
+    // -----------------------------------------------------------------
+
+    new Draggable(containerEl, {
+      itemSelector: '.external-event',
+      eventData: function(eventEl) {
+        console.log(eventEl);
+        return {
+          title: eventEl.innerText,
+          backgroundColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
+          borderColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
+          textColor: window.getComputedStyle( eventEl ,null).getPropertyValue('color'),
+        };
+      }
+    });
+
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ 'bootstrap', 'interaction', 'dayGrid', 'timeGrid' ],
+      header    : {
+        left  : 'prev,next today',
+        center: 'title',
+        right : 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      //Random default events
+      events    : [
+        {
+          title          : 'All Day Event',
+          start          : new Date(y, m, 1),
+          backgroundColor: '#f56954', //red
+          borderColor    : '#f56954' //red
+        },
+        {
+          title          : 'Long Event',
+          start          : new Date(y, m, d - 5),
+          end            : new Date(y, m, d - 2),
+          backgroundColor: '#f39c12', //yellow
+          borderColor    : '#f39c12' //yellow
+        },
+        {
+          title          : 'Meeting',
+          start          : new Date(y, m, d, 10, 30),
+          allDay         : false,
+          backgroundColor: '#0073b7', //Blue
+          borderColor    : '#0073b7' //Blue
+        },
+        {
+          title          : 'Lunch',
+          start          : new Date(y, m, d, 12, 0),
+          end            : new Date(y, m, d, 14, 0),
+          allDay         : false,
+          backgroundColor: '#00c0ef', //Info (aqua)
+          borderColor    : '#00c0ef' //Info (aqua)
+        },
+        {
+          title          : 'Birthday Party',
+          start          : new Date(y, m, d + 1, 19, 0),
+          end            : new Date(y, m, d + 1, 22, 30),
+          allDay         : false,
+          backgroundColor: '#00a65a', //Success (green)
+          borderColor    : '#00a65a' //Success (green)
+        },
+        {
+          title          : 'Click for Google',
+          start          : new Date(y, m, 28),
+          end            : new Date(y, m, 29),
+          url            : 'http://google.com/',
+          backgroundColor: '#3c8dbc', //Primary (light-blue)
+          borderColor    : '#3c8dbc' //Primary (light-blue)
+        }
+      ],
+      editable  : true,
+      droppable : true, // this allows things to be dropped onto the calendar !!!
+      drop      : function(info) {
+        // is the "remove after drop" checkbox checked?
+        if (checkbox.checked) {
+          // if so, remove the element from the "Draggable Events" list
+          info.draggedEl.parentNode.removeChild(info.draggedEl);
+        }
+      }    
+    });
+
+    calendar.render();
+    // $('#calendar').fullCalendar()
+
+    /* ADDING EVENTS */
+    var currColor = '#3c8dbc' //Red by default
+    //Color chooser button
+    var colorChooser = $('#color-chooser-btn')
+    $('#color-chooser > li > a').click(function (e) {
+      e.preventDefault()
+      //Save color
+      currColor = $(this).css('color')
+      //Add color effect to button
+      $('#add-new-event').css({
+        'background-color': currColor,
+        'border-color'    : currColor
+      })
+    })
+    $('#add-new-event').click(function (e) {
+      e.preventDefault()
+      //Get value and make sure it is not null
+      var val = $('#new-event').val()
+      if (val.length == 0) {
+        return
+      }
+
+      //Create events
+      var event = $('<div />')
+      event.css({
+        'background-color': currColor,
+        'border-color'    : currColor,
+        'color'           : '#fff'
+      }).addClass('external-event')
+      event.html(val)
+      $('#external-events').prepend(event)
+
+      //Add draggable funtionality
+      ini_events(event)
+
+      //Remove event from text input
+      $('#new-event').val('')
+    })
+  })
+</script>
+</body>
+</html>
