7f7bf414769792c28a9dd77d190843f86ad6a7fb
Innovatah <innovatah.dev@localhost.localdomain>
2019-11-14 08:21:16 +0300

    revert nullable is id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
+            $table->bigIncrements('id');
             $table->string('pf_number')->nullable();
             $table->string('first_name')->nullable();
             $table->string('second_name')->nullable();
